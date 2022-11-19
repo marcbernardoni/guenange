fea5ad5cae267ad21eca198dd039cb8ab148b52f
Marc Bernardoni <marc.bernardoni@gmail.com>
2022-11-19 18:54:07 +0100

    (feat) InputHandler
    Mise en place de la gestion des coordonnées de la souris avec préparation d'un mode debug

--- dist/class/InputHandler.js
+++ dist/class/InputHandler.js
@@ -1,1 +1,31 @@
-export default class InputHandler {}
+export default class InputHandler {
+  constructor(game) {
+    this.game = game
+    this.width = 50
+    this.height = 50
+    this.elementX = document.querySelector('.coordX')
+    this.elementY = document.querySelector('.coordY')
+    this.mouseX = 0
+    this.mouseY = 0
+
+    window.addEventListener('mousemove', (event) => {
+      event.preventDefault()
+      event.stopPropagation()
+      if (event.target.id === 'canvas') {
+        this.mouseX = event.layerX
+        this.mouseY = event.layerY
+      }
+    })
+  }
+
+  // update() {}
+
+  draw(context) {
+    this.elementX.innerHTML = this.mouseX
+    this.elementY.innerHTML = this.mouseY
+
+    context.beginPath()
+    context.arc(this.mouseX, this.mouseY, 20, 0, Math.PI * 2)
+    context.stroke()
+  }
+}
--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,4 @@
-import InputHandler from './class/InputHandler'
-import Layer from './class/Layer'
+import InputHandler from './class/InputHandler.js'
 
 window.addEventListener('load', () => {
   // canvas setup
@@ -8,6 +7,32 @@ window.addEventListener('load', () => {
   canvas.width = 1024
   canvas.height = 768
 
-  const mouse = new InputHandler(ctx)
-  const layer = new Layer(ctx)
+  class Game {
+    constructor(width, height) {
+      this.width = width
+      this.height = height
+      this.mouse = new InputHandler(this)
+    }
+
+    update() {}
+
+    draw(context) {
+      this.mouse.draw(context)
+      context.clearRect(0, 0, canvas.width, canvas.heigth)
+    }
+  }
+
+  const game = new Game(
+    canvas.width,
+    canvas.height,
+    canvas.offsetLeft,
+    canvas.offsetTop
+  )
+  function animate() {
+    requestAnimationFrame(animate)
+    game.update()
+    game.draw(ctx)
+  }
+
+  animate()
 })
